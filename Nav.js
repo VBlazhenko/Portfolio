7808524e9f40f63ee8439d6f104aaea3316e2ccb
Vladyslav Blazhenko <78543274+VBlazhenko@users.noreply.github.com>
2023-05-14 13:30:35 +0100

    First update

---
+++ Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {BiHomeAlt, BiUser} from "react-icons/bi";
+import {BsClipboardData, BsBriefcase, BsChatSquare} from "react-icons/bs";
+
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+
+export default Nav;
